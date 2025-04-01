01280113486c30d1a8fb0bb1e3bb1db4762d1c2e
Griffin <griffindannerdoran@gmail.com>
2025-04-01 16:00:54 -0400

    Updated utility to add a class for creating piece-wise hashes and removed un-needed message hashing.

--- Packet.py
+++ Packet.py
@@ -3,17 +3,19 @@ import struct
 from enum import Enum
 import Utility
 
+
 class PacketType(Enum):
-  CHOKE = 0
-  UNCHOKE = 1
-  INTERESTED = 2
-  NOT_INTERESTED = 3
-  HAVE = 4
-  BITFIELD = 5
-  REQUEST = 6
+    CHOKE = 0
+    UNCHOKE = 1
+    INTERESTED = 2
+    NOT_INTERESTED = 3
+    HAVE = 4
+    BITFIELD = 5
+    REQUEST = 6
+
 
 def create_packet(packet_type, *args):
-  """Creates packets depending on what type you submit
+    """Creates packets depending on what type you submit
   Args:
     packet_type (int): Integer 0 - 6 to indicate type of packet  
       0 Choke Message - No extra arguments  
@@ -28,79 +30,81 @@ def create_packet(packet_type, *args):
   Returns:
       packet + packet hashcode ready to be sent  
   """
-  packet = None
-  if packet_type in [0,1,2,3]:
-    packet = struct.pack(">IB", 1, packet_type)
-  elif packet_type == 4:
-    if len(args) != 1:
-      raise ValueError("Have type requires piece_index (int) argument")
-    index = args[0]
-    packet = struct.pack(">IBI", 5, packet_type, index)
-  elif packet_type == 5:
-    if len(args) != 1:
-      raise ValueError("Bitfield type requires bitfield(bytes) argument")
-    bitfield_bytes = args[0]
-    packet = struct.pack(">IB", 1 + len(bitfield_bytes), packet_type) + bitfield_bytes
-  elif packet_type == 6:
-    if len(args) != 2:
-      raise ValueError("Request type requires piece index (int) and file_id (int)")
-    piece_index, file_id = args
-    packet = struct.pack(">IBII", 9, packet_type, piece_index, file_id)
-  else:
-    raise ValueError("Not valid packet type")
-  
-  packet_hash = hashlib.sha256(packet[4:]).digest()
-  return packet + packet_hash
+    packet = None
+    if packet_type in [0, 1, 2, 3]:
+        packet = struct.pack(">IB", 1, packet_type)
+    elif packet_type == 4:
+        if len(args) != 1:
+            raise ValueError("Have type requires piece_index (int) argument")
+        index = args[0]
+        packet = struct.pack(">IBI", 5, packet_type, index)
+    elif packet_type == 5:
+        if len(args) != 1:
+            raise ValueError("Bitfield type requires bitfield(bytes) argument")
+        bitfield_bytes = args[0]
+        packet = struct.pack(">IB", 1 + len(bitfield_bytes),
+                             packet_type) + bitfield_bytes
+    elif packet_type == 6:
+        if len(args) != 2:
+            raise ValueError(
+                "Request type requires piece index (int) and file_id (int)")
+        piece_index, file_id = args
+        packet = struct.pack(">IBII", 9, packet_type, piece_index, file_id)
+    else:
+        raise ValueError("Not valid packet type")
+    return packet
+
 
 def parse_packet(packet):
-  """Function to return a parsed packet
+    """Function to return a parsed packet
   Args:
       packet (byte string): a byte string representation of the packet
   Raises:
       ValueError: If invalid length or packet_type code
   Returns:
       Packet object: {"type": packet_type, "payload": None or data, "valid_hash": boolean}
-  """ 
-  length, = struct.unpack(">I", packet[:4])
-  if len(packet) < length + 32:
-    raise ValueError("Packet length mismatch")
-  
-  packet_type, = struct.unpack(">B", packet[4:5])
-  
-  payload = None
-  if packet_type in [0, 1, 2, 3]:  # Choke, Unchoke, Interested, Not Interested have no payload
+  """
+    length, = struct.unpack(">I", packet[:4])
+    if len(packet) < length:
+        raise ValueError("Packet length mismatch")
+
+    packet_type, = struct.unpack(">B", packet[4:5])
+
     payload = None
-  elif packet_type == 4:  # Have Message
-    payload = struct.unpack(">I", packet[5:9])[0]
-  elif packet_type == 5:  # Bitfield Message
-    bit_string = Utility.bytes_to_binary(packet[5:length+4])
-    payload = bit_string
-  elif packet_type == 6:  # Request Message
-    packet_index, file_id = struct.unpack(">II", packet[5:13])
-    payload = {"packet_index": packet_index, "file_id": file_id}
-  else:
-    raise ValueError("Invalid packet type")
-  
-  received_hash = packet[4 + length:4 + length + 32]  # Last 32 bytes
-  computed_hash = hashlib.sha256(packet[4:4 + length]).digest()    
-  return {
-      "type": PacketType(packet_type).name,
-      "payload": payload,
-      "valid_hash": computed_hash == received_hash
-  }
+    if packet_type in [0, 1, 2,
+                       3]:  # Choke, Unchoke, Interested, Not Interested have no payload
+        payload = None
+    elif packet_type == 4:  # Have Message
+        payload = struct.unpack(">I", packet[5:9])[0]
+    elif packet_type == 5:  # Bitfield Message
+        bit_string = Utility.bytes_to_binary(packet[5:length + 4])
+        payload = bit_string
+    elif packet_type == 6:  # Request Message
+        packet_index, file_id = struct.unpack(">II", packet[5:13])
+        payload = {"packet_index": packet_index, "file_id": file_id}
+    else:
+        raise ValueError("Invalid packet type")
+    return {
+        "type": PacketType(packet_type).name,
+        "payload": payload
+    }
+
 
 if __name__ == "__main__":
-  index = 100
-  example_packet = create_packet(4, index)  # Create a have packet
-  parsed_data = parse_packet(example_packet)
-  print(parsed_data)
-  
-  bitfield_bytes = Utility.create_bitfield({5: "test", 1: "test2", 4:"test3"}, 10)
-  example_packet = create_packet(5, bitfield_bytes)  # Create a bitfield packet
-  parsed_data = parse_packet(example_packet)
-  print(parsed_data)
-  
-  packet_index, file_id = 10, 15
-  example_packet = create_packet(6, packet_index, file_id)  # Create a request packet
-  parsed_data = parse_packet(example_packet)
-  print(parsed_data)
+    index = 100
+    example_packet = create_packet(4, index)  # Create a have packet
+    parsed_data = parse_packet(example_packet)
+    print(parsed_data)
+
+    bitfield_bytes = Utility.create_bitfield(
+        {5: "test", 1: "test2", 4: "test3"}, 10)
+    example_packet = create_packet(5,
+                                   bitfield_bytes)  # Create a bitfield packet
+    parsed_data = parse_packet(example_packet)
+    print(parsed_data)
+
+    packet_index, file_id = 10, 15
+    example_packet = create_packet(6, packet_index,
+                                   file_id)  # Create a request packet
+    parsed_data = parse_packet(example_packet)
+    print(parsed_data)
--- Utility.py
+++ Utility.py
@@ -1,65 +1,86 @@
+import hashlib
 import os
 
-def split_file(folder, filename, packet_size):
-  """Splits a file into chunks of a given size.
+
+def split_file(folder, filename, piece_size):
+    """Splits a file into chunks of a given size.
   Args:
     folder (str): Folder with file.
     filename (str): File to split.
+    piece_size (int): Size of each file piece.
   Returns:
-      list: byte chunks of file
+      list: byte pieces of file
   """
-  local_files = os.path.join(folder, filename)
-  chunks = []
-  with open(local_files, "rb") as file:
-    while chunk := file.read(packet_size):
-      chunks.append((chunk).ljust(packet_size, b'\0')) # If < packet size, fill in with buffer '\0'
-  return chunks
+    local_files = os.path.join(folder, filename)
+    pieces = []
+    with open(local_files, "rb") as file:
+        while piece := file.read(piece_size):
+            # If < packet size, fill in with buffer '\0'
+            pieces.append(piece.ljust(piece_size, b'\0'))
+    return pieces
+
+
+def piece_hash(pieces):
+    """Calculates the piecewise hashes of the provided file bytes.
+    Args:
+      pieces (list): list of byte segments corresponding to pieces.
+    Returns:
+        list: bytes corresponding to the hash of each piece
+    """
+    piece_hashes = []
+    for piece in pieces:
+        piece_hashes.append(hashlib.sha256(piece))
+    return piece_hashes
+
 
 def reassemble_file(pieces, folder, output_filename):
-  """Reassemble file given byte chunks.
+    """Reassemble file given byte chunks.
   Args:
     pieces (list): Byte list to be reassembled.
     folder (str): Directory to save new file.
     output_filename (str): Name of new file.
   """
-  remote_files = os.path.join(folder, output_filename)
-  #TODO maybe check total hash in this function before actually writing it locally
-  with open(remote_files, "wb") as file:
-    for piece in pieces:
-      file.write(piece)
-      
+    remote_files = os.path.join(folder, output_filename)
+    # TODO maybe check total hash in this function before actually writing it locally
+    with open(remote_files, "wb") as file:
+        for piece in pieces:
+            file.write(piece)
+
+
 def create_bitfield(pieces, file_length):
-  """Create a bitfield for the pieces dictionary for the specified file 
+    """Create a bitfield for the pieces dictionary for the specified file
   Args:
     pieces (dictionary): file = {index: string}
     file_length (int): total length of the file
   Returns:
     byte bitfield
   """
-  bit_array = ["0"] * file_length
-  for index in list(pieces.keys()):
-    bit_array[index] = "1"
-  string_of_bits = "".join(bit_array)
-  
-  bitfield = string_of_bits.ljust((len(string_of_bits) + 7) // 8 * 8, '0')
-    
-  bitfield_bytes = bytearray()
-  for i in range(0, len(bitfield), 8):
-    byte = bitfield[i:i+8]
-    bitfield_bytes.append(int(byte, 2))
-  
-  return bytes(bitfield_bytes)
+    bit_array = ["0"] * file_length
+    for index in list(pieces.keys()):
+        bit_array[index] = "1"
+    string_of_bits = "".join(bit_array)
+
+    bitfield = string_of_bits.ljust((len(string_of_bits) + 7) // 8 * 8, '0')
+
+    bitfield_bytes = bytearray()
+    for i in range(0, len(bitfield), 8):
+        byte = bitfield[i:i + 8]
+        bitfield_bytes.append(int(byte, 2))
+
+    return bytes(bitfield_bytes)
+
 
 def bytes_to_binary(byte_data):
-  """Converts byte into bit string. Used for bitfield
+    """Converts byte into bit string. Used for bitfield
   Args:
       byte_data (byte): bytes
 
   Returns:
       string: string of bits from bytes
   """
-  return ''.join(f'{byte:08b}' for byte in byte_data)
+    return ''.join(f'{byte:08b}' for byte in byte_data)
+
 
 if __name__ == "__main__":
-  """Just in here for testing purposes, not the cleanest, but can be deleted before we submit"""
-  print(create_bitfield({5: "test", 1: "test2", 4:"test3"}, 10))
+    """Just in here for testing purposes, not the cleanest, but can be deleted before we submit"""
+    print(create_bitfield({5: "test", 1: "test2", 4: "test3"}, 10))
